d738d444f62091aa0d5930b18fc24ca24c2fcb73
YanaCherkes <58664366+YanaCherkes@users.noreply.github.com>
2019-12-14 17:12:28 +0200

    Add files via upload

--- Controller_Generator/block_help_generator.py
+++ Controller_Generator/block_help_generator.py
@@ -49,7 +49,7 @@ def init_state(file, numState, dest_vector, row_size, processing_area_init, num_
         elif int(i) < int(num_of_inputs):
             continue;
         else:
-         calculated_column = int(processing_area_init) + int(i) - int(num_of_inputs) #calculaate the column to initialize
+         calculated_column = int(i) - int(num_of_inputs) #calculaate the column to initialize
          file.writelines(
     "						COLUMN_OUT("+ str(calculated_column) + " + CONV_INTEGER(process_area_in)) <= Vw1; \n");
      #initiate rows
